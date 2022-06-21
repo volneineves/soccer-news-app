812df6e131b53d0829e3e53ffc8571e495b4e079
Volnei Neves <volneinevesfilho@hotmail.com>
2022-06-21 20:37:31 -0300

    Criando RecycleView com CardView e Adapter

--- app/src/main/java/com/volnei/soccernews/ui/home/NewsFragment.java
+++ app/src/main/java/com/volnei/soccernews/ui/home/NewsFragment.java
@@ -9,8 +9,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.volnei.soccernews.databinding.FragmentNewsBinding;
+import com.volnei.soccernews.ui.adapter.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
@@ -18,14 +20,13 @@ public class NewsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel =
-                new ViewModelProvider(this).get(NewsViewModel.class);
+        NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNews;
-        newsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
+        newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> binding.rvNews.setAdapter(new NewsAdapter(news)));
         return root;
     }
 
--- app/src/main/java/com/volnei/soccernews/ui/home/NewsViewModel.java
+++ app/src/main/java/com/volnei/soccernews/ui/home/NewsViewModel.java
@@ -4,16 +4,29 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.volnei.soccernews.domain.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
+        news = new MutableLiveData<>();
+        List<News> news = new ArrayList<>();
+
+        // TODO Remover Mock de notícias
+        news.add(new News("Ferroviária tem desfalque importante", "Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"));
+        news.add(new News("Ferrinha joga no sábado", "Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"));
+        news.add(new News("Copa do Mundo está terminando", "Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"));
+
+        this.news.setValue(news);
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
